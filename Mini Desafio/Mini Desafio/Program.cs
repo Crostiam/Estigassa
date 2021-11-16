0b01b4f72d3b7cb7d6af228309ad90702c553467
Cristian Felicio <94009542+Crostiam@users.noreply.github.com>
2021-11-16 10:50:09 +0000

    Add files via upload

---
+++ Mini Desafio/Mini Desafio/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c, d, e, f, g, h, i, j;
+
+
+            Console.WriteLine("Color:____");
+            a = Console.ReadLine();
+            Console.WriteLine("Noun:____");
+            b = Console.ReadLine();
+            Console.WriteLine("Food:____");
+            c = Console.ReadLine();
+            Console.WriteLine("Adjetive:____");
+            d = Console.ReadLine();
+            Console.WriteLine("Adjetive:____");
+            e = Console.ReadLine();
+            Console.WriteLine("Number:____");
+            f = Console.ReadLine();
+            Console.WriteLine("Noun:____");
+            g = Console.ReadLine();
+            Console.WriteLine("Verb:____");
+            h = Console.ReadLine();
+            Console.WriteLine("Verb:____");
+            i = Console.ReadLine();
+            Console.WriteLine("Noun:____");
+            j = Console.ReadLine();
+
+
+            Console.WriteLine("1. Select the type of bread you want to use. Many prefer the taste of " + a + " bread, while others prefer " + b + " bread because it is healthy.");
+            Console.WriteLine("2.Choose the flavor of Jam / Jelly.I personally prefer " + c + " jam, but you can use whatever you want.");
+            Console.WriteLine("3.Choose the type of penut butter - either " + d + " or " + e + ".");
+            Console.WriteLine("4.Take out " + f + " slice(s) of bread.");
+            Console.WriteLine("5.Use a " + g + " to " + h + " the jam all over on of the pieces of bread.");
+            Console.WriteLine("6.Now " + i + " the penut butter on the other piece of bread.");
+            Console.WriteLine("7.Put them together, and you have a PB & J " + j + ".");
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
